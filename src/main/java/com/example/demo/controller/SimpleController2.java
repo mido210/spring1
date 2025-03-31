d9a4d5d6e8d57ba86aa42cca358d90dcd541e2b6
mido210 <midoriya2109@gmail.com>
2025-03-31 14:02:39 +0900

    프로젝트 최초 공유

---
+++ src/main/java/com/example/demo/controller/SimpleController2.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.*;
+
+@Controller
+public class SimpleController2 {
+    //test1로 들어오면 타임리프 test1.html로 보내는 컨트롤러 메소드
+    //Model : 데이터, view : 화면
+    //컨트롤러는 Model과 View를 연결하고 제어 -> 그래서 이름이 Controller
+    // 자바 웹은 Model, View, Controller로 구성 -> MVC 패턴
+    @GetMapping("/test1")
+    public ModelAndView test1(){
+        return new ModelAndView("test1");
+    }
+    @GetMapping("/test2")
+    public ModelAndView test2(){
+        return new ModelAndView("test2");
+    }
+    @GetMapping("/test3")
+    public ModelAndView test3(){
+        return new ModelAndView("test3").addObject("message", "Hello World");
+    }
+    // 두개의 정수를 test4뷰로 보낸다
+    // test4 뷰는 두 정수를 더해서 출력한다
+    @GetMapping("/test4")
+    public ModelAndView test4(){
+        int a = 10;
+        int b = 20;
+        return new ModelAndView("test4").addObject("a",a).addObject("b",b);
+    }
+}
